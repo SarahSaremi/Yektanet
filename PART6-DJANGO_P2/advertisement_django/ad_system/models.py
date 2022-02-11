5f9c7e0fa8f429f24100b3ad24bbc1b6dd9e0c45
Sarah <sarasaaremi@gmail.com>
2022-02-11 14:35:30 +0330

    authentication

--- PART6-DJANGO_P2/advertisement_django/ad_system/models.py
+++ PART6-DJANGO_P2/advertisement_django/ad_system/models.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 
 class Advertiser(models.Model):
@@ -52,3 +56,9 @@ class Click(BaseStat):
 class View(BaseStat):
     def __str__(self):
         return f'{self.ad.title} viewed.'
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- PART6-DJANGO_P2/advertisement_django/ad_system/urls.py
+++ PART6-DJANGO_P2/advertisement_django/ad_system/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken import views as auth_views
 
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path('', views.AdsView.as_view(), name='index'),
     path('report/', views.ReportView.as_view(), name='report'),
     path('click/<int:ad_id>/', views.AdRedirectView.as_view(), name='ad_details'),
+    # path('api-token-auth/', auth_views.obtain_auth_token)
 ]
